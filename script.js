3800eba921bfa8d98a1902c1d42806695ca18e4a
saqlvation <volpetta0987@gmail.com>
2026-06-19 00:10:20 +0200

    fixed lag, new world event counter

--- script.js
+++ script.js
@@ -10,11 +10,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let lat = 0;
 let lon = 0;
+const numberDisasters = document.getElementById("numberDisasters");
 fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=30') // this is set to "limit=30" because it's tracking a lot of unclosed events, which would lag the whole page
 .then(response => response.json())
-.then(data => data.events.forEach(data => {
-    lon = data.geometry[0].coordinates[0] // this is the longitude
-    lat = data.geometry[0].coordinates[1] // this is the latitude
-    L.marker([lat,lon]).addTo(map);
+.then(data => {
+    numberDisasters.textContent = data.events.length;
+    data.events.forEach(event => {
+    lon = event.geometry[0].coordinates[0] // this is the longitude
+    lat = event.geometry[0].coordinates[1] // this is the latitude
+    L.marker([lat,lon]).addTo(map); // adds a simple marker ( like the google maps one..)
 
-}));
+    });
+});
+
