94c0f314356c2da5c6ac23f7baa5e4cfd51772aa
saqlvation <volpetta0987@gmail.com>
2026-06-18 23:53:48 +0200

    working markers and set a limit to 30 events

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open')
+let lat = 0;
+let lon = 0;
+fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=30') // this is set to "limit=30" because it's tracking a lot of unclosed events, which would lag the whole page
 .then(response => response.json())
-.then(data => console.log(data.events));
+.then(data => data.events.forEach(data => {
+    lon = data.geometry[0].coordinates[0] // this is the longitude
+    lat = data.geometry[0].coordinates[1] // this is the latitude
+    L.marker([lat,lon]).addTo(map);
+
+}));
