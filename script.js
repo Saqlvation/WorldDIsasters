a72b857f7fa7ace8d5432384ffa897f0e4828191
saqlvation <volpetta0987@gmail.com>
2026-06-20 19:02:36 +0200

    added a marker popup title

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let lat = 0;
 let lon = 0;
 const numberDisasters = document.getElementById("numberDisasters");
-fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=30') // this is set to "limit=30" because it's tracking a lot of unclosed events, which would lag the whole page
+fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=100') // this is set to "limit=30" because it's tracking a lot of unclosed events, which would lag the whole page
 .then(response => response.json())
 .then(data => {
     numberDisasters.textContent = data.events.length;
     data.events.forEach(event => {
     lon = event.geometry[0].coordinates[0] // this is the longitude
     lat = event.geometry[0].coordinates[1] // this is the latitude
-    L.marker([lat,lon]).addTo(map); // adds a simple marker ( like the google maps one..)
+    L.marker([lat, lon]).bindPopup(event.title).addTo(map); // adds a simple marker ( like the google maps one..)
 
     });
 });
