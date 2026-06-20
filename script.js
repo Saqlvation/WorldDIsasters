366071d56040f6658890e548eca3ab591a5cf0bc
saqlvation <volpetta0987@gmail.com>
2026-06-20 19:24:22 +0200

    added differencation with if statements  and icons

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ let wildfireIcon = L.icon({
     iconUrl: 'assets/wildfire.png',
     iconSize: [32, 32],
 });
+let iceIcon = L.icon({
+    iconUrl: 'assets/ice.png',
+    iconSize: [32, 32],
+});
 const numberDisasters = document.getElementById("numberDisasters");
 fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=100') // this is set to "limit=30" because it's tracking a lot of unclosed events, which would lag the whole page
 .then(response => response.json())
@@ -28,7 +32,15 @@ fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=100') // this
     lon = event.geometry[0].coordinates[0] // this is the longitude
     lat = event.geometry[0].coordinates[1] // this is the latitude
     // adds a simple marker ( like the google maps one..) (with marker addto) while bindpopup is what happens when you click on it it will show you all the descriptions
-    L.marker([lat, lon],{ icon: volcanoIcon }).bindPopup(`
+let icon;
+if (event.categories[0].title === "Wildfires") {
+    icon = wildfireIcon;
+} else if (event.categories[0].title === "Volcanoes") {
+    icon = volcanoIcon;
+} else if (event.categories[0].title === "Sea and Lake Ice") {
+    icon = iceIcon;
+}
+    L.marker([lat, lon],{ icon: icon }).bindPopup(`
     <b>${event.title}</b>
     <p>Category: ${event.categories[0].title}</p>
     <a href="${event.link}">More info</a>
