baab57220ab9b0560dd650a7bfaa2029a3b5d92c
saqlvation <volpetta0987@gmail.com>
2026-06-21 20:03:07 +0200

    sidebar and different images

--- script.js
+++ script.js
@@ -24,7 +24,12 @@ let iceIcon = L.icon({
     iconUrl: 'assets/ice.png',
     iconSize: [32, 32],
 });
+let defaultIcon = L.icon({
+    iconUrl: 'assets/default.png',
+    iconSize: [32, 32],
+});
 const numberDisasters = document.getElementById("numberDisasters");
+const sidebar = document.getElementById("sidebar");
 fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=100') // this is set to "limit=30" because it's tracking a lot of unclosed events, which would lag the whole page
 .then(response => response.json())
 .then(data => {
@@ -40,13 +45,20 @@ if (event.categories[0].title === "Wildfires") {
     icon = volcanoIcon;
 } else if (event.categories[0].title === "Sea and Lake Ice") {
     icon = iceIcon;
+} else {
+    icon = defaultIcon;
 }
     L.marker([lat, lon],{ icon: icon }).bindPopup(`
     <b>${event.title}</b>
     <p>Category: ${event.categories[0].title}</p>
     <a href="${event.link}">More info</a>
 `).addTo(map); 
+    let item = document.createElement('div');
+    item.textContent = event.title;
+    sidebar.appendChild(item);
 
     });
+    
 });
+
 
